495049b1295bde80177ba5fa4528710f7d9204f6
enzo filippo <enzo.filippo23@gmail.com>
2023-07-29 11:26:45 +1200

    profile update func added in relation with supabase

---
+++ app/(app)/profile/components/InputContainer.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { HalfCircleSpinner } from "react-epic-spinners";
+import { Target, UpdatesParams } from "../page";
+
+type ContainerProps = {
+  children: React.ReactNode;
+  target: Target;
+  isUpdated: boolean;
+  isSaving: boolean;
+  handleUpdates: (params: UpdatesParams) => Promise<void>;
+};
+
+export function InputContainer({ ...props }: ContainerProps) {
+  return (
+    <div className="flex flex-col gap-2">
+      {props.children}
+
+      {props.isUpdated && (
+        <div className="flex gap-4 self-end text-sm">
+          <button
+            onClick={() =>
+              props.handleUpdates({ type: "cancel", target: props.target })
+            }
+            className="uppercase font-mono font-semibold text-red-600"
+            disabled={props.isSaving}
+          >
+            cancel
+          </button>
+          <button
+            onClick={() =>
+              props.handleUpdates({ type: "save", target: props.target })
+            }
+            className="grid place-items-center uppercase font-mono font-semibold text-neutral-100 border rounded-sm px-2 min-w-[50px] h-6"
+          >
+            {props.isSaving ? <HalfCircleSpinner size={16} /> : "save"}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
--- app/(app)/profile/page.tsx
+++ app/(app)/profile/page.tsx
@@ -7,12 +7,20 @@ import {
   TextArea,
 } from "@/app/components/Inputs";
 import { useProfiles } from "@/supabase/profiles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { InputContainer } from "./components/InputContainer";
+import { supabaseClient } from "@/supabase/client";
+
+export type Target = "name" | "role" | "location" | "experience" | "biog";
+
+export type UpdatesParams = {
+  type: "cancel" | "save";
+  target: Target;
+};
 
 export default function ProfilePage() {
   const {
     user_profile: {
-      auth_id,
       bio,
       email,
       full_name,
@@ -38,6 +46,116 @@ export default function ProfilePage() {
   const [biog, setBiog] = useState<string>(bio);
   const [biogUpd, setBiogUpd] = useState<boolean>(false);
 
+  const [isSaving, setIsSaving] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (username !== full_name) setNameUpd(true);
+    if (username === full_name) setNameUpd(false);
+  }, [username, full_name]);
+
+  useEffect(() => {
+    if (role !== primary_role) setRoleUpd(true);
+    if (role === primary_role) setRoleUpd(false);
+  }, [role, primary_role]);
+
+  useEffect(() => {
+    if (loc !== location) setLocUpd(true);
+    if (loc === location) setLocUpd(false);
+  }, [loc, location]);
+
+  useEffect(() => {
+    if (experience !== years_experience) setExperienceUpd(true);
+    if (experience === years_experience) setExperienceUpd(false);
+  }, [experience, years_experience]);
+
+  useEffect(() => {
+    if (biog !== bio) setBiogUpd(true);
+    if (biog === bio) setBiogUpd(false);
+  }, [biog, bio]);
+
+  async function handleUpdates(params: UpdatesParams) {
+    const { type, target } = params;
+
+    if (type === "save") await saveUpdates(target);
+
+    if (type === "cancel") {
+      switch (target) {
+        case "name":
+          setUsername(full_name);
+          break;
+
+        case "role":
+          setRole(primary_role);
+          break;
+
+        case "location":
+          setLoc(location);
+          break;
+
+        case "experience":
+          setExperience(years_experience);
+          break;
+
+        case "biog":
+          setBiog(bio);
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  async function saveUpdates(target: Target) {
+    const supabase = supabaseClient();
+    setIsSaving(true);
+
+    switch (target) {
+      case "name":
+        await supabase
+          .from("profiles")
+          .update({ full_name: username })
+          .eq("profile_id", profile_id);
+        setIsSaving(false);
+        break;
+
+      case "role":
+        await supabase
+          .from("profiles")
+          .update({ primary_role: role })
+          .eq("profile_id", profile_id);
+        setIsSaving(false);
+        break;
+
+      case "location":
+        await supabase
+          .from("profiles")
+          .update({ location: loc })
+          .eq("profile_id", profile_id);
+        setIsSaving(false);
+        break;
+
+      case "experience":
+        await supabase
+          .from("profiles")
+          .update({ years_experience: experience })
+          .eq("profile_id", profile_id);
+        setIsSaving(false);
+        break;
+
+      case "biog":
+        await supabase
+          .from("profiles")
+          .update({ bio: biog })
+          .eq("profile_id", profile_id);
+        setIsSaving(false);
+        break;
+
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="flex flex-col gap-10 place-self-center border border-neutral-50 rounded-sm p-4 mt-4 w-full max-w-5xl sm:p-6 sm:mt-0">
       <section className="flex flex-col gap-6 sm:flex-row sm:gap-8">
@@ -54,55 +172,87 @@ export default function ProfilePage() {
         </div>
 
         <div className="flex flex-col flex-1 gap-y-4">
-          <Input
-            id="name"
-            label="Full name"
-            value={username}
-            onChange={setUsername}
-          />
+          <InputContainer
+            target="name"
+            isUpdated={nameUpd}
+            isSaving={isSaving}
+            handleUpdates={handleUpdates}
+          >
+            <Input
+              id="name"
+              label="Full name"
+              value={username}
+              onChange={setUsername}
+            />
+          </InputContainer>
 
-          <Input
-            id="role"
-            label="Primary role"
-            placeholder="Cosmic Potato Farmer"
-            value={role}
-            onChange={setRole}
-          />
+          <InputContainer
+            target="role"
+            isUpdated={roleUpd}
+            isSaving={isSaving}
+            handleUpdates={handleUpdates}
+          >
+            <Input
+              id="role"
+              label="Primary role"
+              placeholder="Cosmic Potato Farmer"
+              value={role}
+              onChange={setRole}
+            />
+          </InputContainer>
 
-          <Input
-            id="location"
-            label="Location"
-            placeholder="Topsy-Turvy"
-            value={loc}
-            onChange={setLoc}
-          />
+          <InputContainer
+            target="location"
+            isUpdated={locUpd}
+            isSaving={isSaving}
+            handleUpdates={handleUpdates}
+          >
+            <Input
+              id="location"
+              label="Location"
+              placeholder="Topsy-Turvy"
+              value={loc}
+              onChange={setLoc}
+            />
+          </InputContainer>
 
-          {/* Do a dropdown for this one */}
-          <Dropdown
-            id="experience"
-            label="Years of experience"
-            value={experience}
-            onChange={setExperience}
-            options={[
-              { value: "0", label: "None" },
-              { value: "1", label: "1 year" },
-              { value: "over 2", label: "> 2 years" },
-              { value: "over 5", label: "> 5 years" },
-              { value: "over 10", label: "> 10 years" },
-            ]}
-          />
+          <InputContainer
+            target="experience"
+            isUpdated={experienceUpd}
+            isSaving={isSaving}
+            handleUpdates={handleUpdates}
+          >
+            <Dropdown
+              id="experience"
+              label="Years of experience"
+              value={experience}
+              onChange={setExperience}
+              options={[
+                { value: "0", label: "None" },
+                { value: "1", label: "1 year" },
+                { value: "over 2", label: "> 2 years" },
+                { value: "over 5", label: "> 5 years" },
+                { value: "over 10", label: "> 10 years" },
+              ]}
+            />
+          </InputContainer>
 
-          {/* Do custom textarea */}
-          <TextArea
-            id="bio"
-            label={`About ${username.trim().split(" ")[0]}`}
-            placeholder="Here, on the brink of the Martian frontier, I dream of forging a new path for humanity."
-            value={biog}
-            onChange={setBiog}
-            span="Write a few sentences to describe your values and goals."
-          />
+          <InputContainer
+            target="biog"
+            isUpdated={biogUpd}
+            isSaving={isSaving}
+            handleUpdates={handleUpdates}
+          >
+            <TextArea
+              id="bio"
+              label={`About ${username.trim().split(" ")[0]}`}
+              placeholder="Here, on the brink of the Martian frontier, I dream of forging a new path for humanity."
+              value={biog}
+              onChange={setBiog}
+              span="Write a few sentences to describe your values and goals."
+            />
+          </InputContainer>
 
-          {/* Make a disabled input */}
           <DisabledInput
             id="email"
             label="Email address"
