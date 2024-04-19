6aff09849f10277609f04d46b658df096948b88f
enzo filippo <enzo.filippo23@gmail.com>
2024-04-19 13:33:32 +1200

    updated favicon and logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Logo() {
   return (
     <Link
       href="/"
-      className="font-mono font-semibold tracking-wide text-lg lg:text-xl bg-gradient-to-b from-neutral-50 to-neutral-50/70 bg-clip-text text-transparent"
+      className="flex items-center gap-1 font-mono font-semibold tracking-wide text-lg lg:text-xl bg-gradient-to-b from-neutral-50 to-neutral-50/70 bg-clip-text text-transparent"
     >
+      <Image src="/favicon.ico" alt="" width={32} height={32} />
       resumai
     </Link>
   );
